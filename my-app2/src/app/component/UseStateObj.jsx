a8f6670a4fdee48534a687659f01d86f6d4ac143
hitgub123 <657578087@qq.com>
2024-03-19 23:49:32 +0900

    UseStateObj

---
+++ my-app2/src/app/component/UseStateObj.jsx
@@ -0,0 +1,19 @@
+'use client'
+import { useState } from 'react';
+function UseStateObj() {
+  const [obj, setObj] = useState({ id: 1, name: 'no.1' });
+  function modifyObj() { 
+    let id = obj.id;
+    setObj({ id: id+1, name: 'no.'+(id+1) });
+  };
+  return (
+    <>
+      <button onClick={modifyObj}>modify obj</button>
+      <p>{ obj.id + ' >>> ' + obj.name}</p>
+    </>
+  )
+}
+export default UseStateObj;
+
+
+
