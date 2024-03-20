7fec46f6b2f59a0b2487c29989f3aa8f03f06f32
hitgub123 <657578087@qq.com>
2024-03-20 14:22:58 +0900

    x

--- my-app2/src/app/component/Profile.tsx
+++
@@ -1,9 +0,0 @@
-function Profile() {
-  return (
-    <img
-      src="https://konachan.net/data/preview/9c/47/9c4749a49aa0c34af7f6fcfba3543ed8.jpg"
-      alt="wife3"
-    />
-  );
-}
-export default Profile;
