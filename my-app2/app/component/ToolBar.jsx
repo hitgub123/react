a71af252690786266b1a2fb7130d143c8d02d35a
hitgub123 <657578087@qq.com>
2024-03-20 14:46:52 +0900

    1

--- my-app2/app/component/ToolBar.jsx
+++ my-app2/app/component/ToolBar.jsx
@@ -1,15 +1,19 @@
-'use client'
-function hello() { alert('hello') }
-function world(){alert('world')}
+"use client";
+function hello() {
+  alert("hello");
+}
+function world() {
+  alert("world");
+}
 function Button1({ callBack, children }) {
-  return (<button onClick={callBack}>{children}</button>);
+  return <button onClick={callBack}>{children}</button>;
 }
 function ToolBar() {
   return (
     <div>
-      <Button1 callBack={hello} children="say hello"/><br/>
-      <Button1 callBack={world} children="say world"/><br/>
+      <Button1 callBack={hello}> { "say hello"}</Button1> <br />
+      <Button1 callBack={world}>{ "say world"}</Button1> <br />
     </div>
-  )
+  );
 }
 export default ToolBar;
--- my-app2/app/page.tsx
+++ my-app2/app/page.tsx
@@ -1,16 +1,17 @@
 // "use server"
+/* eslint react/no-unescaped-entities */
 import Gallery from "./component/Gallery";
 import IfElseCom from "./component/IfElseDemo";
 import ListCom from "./component/ListDemo";
 import Snapshot from "./component/Snapshot";
 import ToolBar from "./component/ToolBar";
+import UseImmerObj from "./component/UseImmerObj";
 import UseStateDemo1 from "./component/UseStateDemo1";
+import UseStateListAdd from "./component/UseStateListAdd";
+import UseStateListAll from "./component/UseStateListAll";
+import UseStateListDel from "./component/UseStateListDel";
 import UseStateObj from "./component/UseStateObj";
 import UseStateObjNest from "./component/UseStateObjNest";
-import UseImmerObj from "./component/UseImmerObj";
-import UseStateListAll from "./component/UseStateListAll";
-import UseStateListAdd from "./component/UseStateListAdd";
-import UseStateListDel from "./component/UseStateListDel";
 export default function Gallery1() {
   const title1 = "New My Lily()";
   const style1 = { color: "red" };
@@ -23,44 +24,42 @@ export default function Gallery1() {
   }
   return (
     <>
-      <h1>Hedy Lamarr's Todos</h1>
-
-      <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        className="photo"
-      ></img>
-
+      <h1>##############################style##############################</h1>
       <ul>
         <li style={{ color: "green" }}>{title1}</li>
         <li style={style1}>today is {formatDate(new Date())}</li>
         <li>Improve the spectrum technology</li>
       </ul>
-
+      <h1>##############################Gallery##############################</h1>
       <a href="https://nextjs.org">Next.js!</a>
       <Gallery person={js1} sex="female" />
 
+      <h1>##############################IfElseCom##############################</h1>
       <section>
         <ul>
           <IfElseCom name={"name1"} flg={true} />
           <IfElseCom name={"name2"} flg={false} />
         </ul>
       </section>
-
+      <h1>##############################ListCom##############################</h1>
       <ListCom exclude="c" />
-
+      <h1>##############################ToolBar##############################</h1>
       <ToolBar />
-
+      <h1>##############################UseStateDemo1##############################</h1>
       <UseStateDemo1 />
-
+      <h1>##############################Snapshot##############################</h1>
       <Snapshot />
-
+      <h1>##############################UseStateObj##############################</h1>
       <UseStateObj />
+      <h1>##############################UseStateObjNest##############################</h1>
       <UseStateObjNest />
+      <h1>##############################UseImmerObj##############################</h1>
       <UseImmerObj />
-
+      <h1>##############################UseStateListAdd##############################</h1>
       <UseStateListAdd />
+      <h1>##############################UseStateListDel##############################</h1>
       <UseStateListDel />
+      <h1>##############################UseStateListAll##############################</h1>
       <UseStateListAll />
     </>
   );
