8875bc7f84cd3370c175fe2622c0b9c3fd1a22a7
hitgub123 <657578087@qq.com>
2024-03-20 14:05:32 +0900

    9

--- my-app2/README.md
+++ my-app2/README.md
@@ -8,3 +8,9 @@ start nextjs project:
     yarn dev
     ```
 
+in UseStateListAll.jsx
+    <button onClick={delArtist(artist.id)}>del</button>
+        will call delArtist when the button is rendered,use next code to prevent it:
+    <button onClick={()=>delArtist(artist.id)}>del</button>
+        but sometimes onClick={()=>XXX} will raise a bug,so u need use onClick={XXX},
+        i dont know why.
--- my-app2/src/app/component/UseStateObj.jsx
+++ my-app2/src/app/component/UseStateObj.jsx
@@ -1,19 +1,20 @@
-'use client'
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 function UseStateObj() {
-  const [obj, setObj] = useState({ id: 1, name: 'no.1' });
-  function modifyObj() { 
+  const [obj, setObj] = useState({ id: 1, name: "no.1" });
+  function modifyObj() {
     let id = obj.id;
-    setObj({ id: id+1, name: 'no.'+(id+1) });
-  };
+    setObj({ id: id + 1, name: "no." + (id + 1) });
+  }
   return (
     <>
       <button onClick={modifyObj}>modify obj</button>
-      <p>{obj.id}{' >>> '}{obj.name}</p>
+      <p>
+        {obj.id}
+        {" >>> "}
+        {obj.name}
+      </p>
     </>
-  )
+  );
 }
 export default UseStateObj;
-
-
-
